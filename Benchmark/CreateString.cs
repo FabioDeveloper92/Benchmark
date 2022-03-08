1af1e91051382df1af577220d888c1c4e7ea8cbb
FabioR <faitel92@hotmail.it>
2022-03-08 20:07:36 +0100

    first commit

---
+++ Benchmark/CreateString.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Collections.Generic;
+using System.Text;
+
+namespace Benchmark
+{
+    public class CreateString
+    {
+        private readonly int _numItem;
+        public CreateString(int numItem)
+        {
+            _numItem = numItem;
+        }
+
+        public string GenerateWithStringBuilder()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < _numItem; i++)
+            {
+                sb.Append("Hello World!" + i);
+            }
+
+            return sb.ToString();
+        } 
+        
+        public string GenerateWithList()
+        {
+            var list = new List<string>(_numItem);
+            for (int i = 0; i < _numItem; i++)
+            {
+                list.Add("Hello World!" + i);
+            }
+
+            return list.ToString();
+        }
+    }
+}
---
+++ Benchmark/CreateStringBenchmark.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser]
+    public class CreateStringBenchmark
+    {
+        // the objs in common of every method, it's better declare global,
+        // in this way we use the same instance and we don't have slowdowns due to creation
+        private const int NUMITEMS = 5000;
+        private static readonly CreateString CreateString = new(NUMITEMS);
+
+        [Benchmark(Baseline = true)]
+        public void GenerateWithStringBuilder()
+        {
+            CreateString.GenerateWithStringBuilder();
+        }  
+        
+        [Benchmark(Baseline = false)]
+        public void GenerateWithList()
+        {
+            CreateString.GenerateWithList();
+        }
+    }
+}
